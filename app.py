280204a147a578e3e451dbb8de8a868e0625c93a
roger11272011 <roger11272011@gmail.com>
2023-06-01 17:05:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -68,6 +68,8 @@ def callback():
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=event.message.text)
+            TextSendMessage(text=event.message.text)
+            TextSendMessage(text=event.message.text)
         )
 
     return 'OK'
