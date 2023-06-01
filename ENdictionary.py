065605ec65bf07fa2dc189e883a99cf0c63241d9
roger11272011 <roger11272011@gmail.com>
2023-06-01 22:47:44 +0800

    Update ENdictionary.py

--- ENdictionary.py
+++ ENdictionary.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 def read(word):
-    url = f'https://tw.dictionary.search.yahoo.com/search;_ylt=Awrt4Gt6H3Bk8tYEtyV9rolQ;_ylc=X1MDMTM1MTIwMDM4MQRfcgMyBGZyA3NmcARmcjIDc2ItdG9wBGdwcmlkA1UxRW5YNnhTUzFLQUF3NmlLTzAuSEEEbl9yc2x0AzAEbl9zdWdnAzEwBG9yaWdpbgN0dy5kaWN0aW9uYXJ5LnNlYXJjaC55YWhvby5jb20EcG9zAzAEcHFzdHIDBHBxc3RybAMwBHFzdHJsAzMEcXVlcnkDeW91BHRfc3RtcAMxNjg1MDY5NzE3?p={word}&fr2=sb-top&fr=sfp'
+    url = f'https://tw.dictionary.search.yahoo.com/search;_ylt=Awrtgeger3hkCoQFOI99rolQ;_ylu=Y29sbwMEcG9zAzEEdnRpZAMEc2VjA3Nj?p={word}&ei=UTF-8&context=gsmcontext%3A%3Alimlangpair%3A%3Aen_zh-hant&fr=sfp'
 
     html = requests.get( url )
     bs = BeautifulSoup(html.text,'lxml')
@@ -11,11 +11,12 @@ def read(word):
         eng = data.find_all('div',class_='fz-16 fl-l dictionaryExplanation')
         ps = data.find_all('div',class_='pos_button fz-14 fl-l mr-12')
         kk = data.find('div',class_='compList d-ib')
-        a=('音標:'+kk.text+'\n'+'-------')
+        a=('音標:'+kk.text+'\n')
 
         for j in range(len(ps)):
-            a=a+('詞性:'+ps[j].text+'\n'+'-------')
-            a=a+('中文:'+eng[j].text+'\n'+'-------')
+            a=a+('詞性:'+ps[j].text+'\n')
+            a=a+('中文:'+eng[j].text+'\n)
+        a=a+'詳細內容:'+url
         return(a)
     except:
         return('查無此單字')
